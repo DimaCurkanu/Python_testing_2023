5aa86d26c19bad8550fcb9b29d8d8410a6bcccfc
DimaCurkanu <kdgrig@gmail.com>
2023-10-04 13:29:55 -0500

    32_refactor

--- 32_cw_capitalize.py
+++ 32_cw_capitalize.py
@@ -2,3 +2,15 @@ import re
 
 def drop_cap(words):
     return re.sub(r'[A-z]{3,}', lambda m: m.group(0).capitalize(),words)
+'''
+our solution: ---> Iryna Kolhanova
+'''
+def drop_cap2(words):
+    upper_case_words = []
+    words2 = words.split(' ')
+    for word in words2:
+        if len(word) > 2:
+            upper_case_words.append(word.capitalize())
+        else:
+            upper_case_words.append(word)
+    return ' '.join(upper_case_words)
