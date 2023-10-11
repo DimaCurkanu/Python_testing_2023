7caedd9f59c1edbfb761e1d712127fddb2199a0d
DimaCurkanu <130791484+DimaCurkanu@users.noreply.github.com>
2023-10-10 22:14:04 -0500

    60_quarks (#30)

---
+++ 60_quarks.py
@@ -0,0 +1,9 @@
+class Quark(object):
+
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+        self.baryon_number = 1.0 / 3
+
+    def interact(self, other_quark):
+        self.color, other_quark.color = other_quark.color, self.color
